c12edb965dbaa77c8b2f563ceac277607999d42e
Rahamtullah8500 <Rahamtullah8500.rahamtullah8500@gmail.com>
2024-08-30 21:59:06 +0530

    modified clinet build path in indes.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,20 +39,15 @@ app.use('/api/templates', templateRouter);
 
 app.use(express.static(path.join(path.resolve(), "public")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(path.resolve(), "client/build")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"));
-//   });
-// }
-
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(path.resolve(), "client/build")));
+
   app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"));
   });
-  }
+}
+
+
 
 const Port = parseInt((process.env.PORT || '5000'),10) 
 
