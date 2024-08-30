f26a5dee9ecbab01f244741fee5f318c185c5cb2
Rahamtullah8500 <Rahamtullah8500.rahamtullah8500@gmail.com>
2024-08-30 21:55:43 +0530

    changed port

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,15 +39,22 @@ app.use('/api/templates', templateRouter);
 
 app.use(express.static(path.join(path.resolve(), "public")));
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(path.resolve(), "client/build")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(path.resolve(), "client/build")));
-
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+  }
 
-const Port = parseInt((process.env.PORT || '4000'),10) 
+const Port = parseInt((process.env.PORT || '5000'),10) 
 
 app.listen(port, () => {
   console.log(`Server running on port ${Port}`);
