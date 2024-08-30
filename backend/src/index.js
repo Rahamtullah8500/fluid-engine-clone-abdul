c4f5c125710924579110dd2f000da74c7bcc4d42
Rahamtullah8500 <Rahamtullah8500.rahamtullah8500@gmail.com>
2024-08-30 22:53:23 +0530

    changed path to index.html

--- backend/src/index.js
+++ backend/src/index.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(path.resolve(), "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"));
+    res.sendFile(path.resolve(path.resolve(), "client", "dist", "index.html"));//build
   });
 }
 
